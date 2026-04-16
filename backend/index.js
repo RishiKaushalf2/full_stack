906a3d9d70127f6acf305669d5b6ef9cc29bf373
K.Rishi Kaushal Reddy <238w1a12f2@vrsec.ac.in>
2026-04-16 15:19:50 +0530

    Changes made

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose")
 const app = express()
 const cors = require("cors")
 require("dotenv").config()
-const authRoutes = require("./routers/authRoutes")
-const PORT = process.env.PORT || 5001
+const authRoutes = require("./routes/authRoutes")
+const productRoutes = require("./routes/productRoutes")
 
 app.use(cors())
 app.use(express.json())
@@ -23,4 +23,5 @@ app.get("/", (req, res) => {
     res.json({ message: "server is running" })
 })
 app.use("/api/auth", authRoutes)
-app.listen(PORT, () => console.log(`server runs on port ${PORT}`))
+app.use("/api/product", productRoutes)
+app.listen(5000, () => console.log("server runns on port 5000"))
