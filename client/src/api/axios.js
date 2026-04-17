c4d72d237a01acb000fa62bd3d9d3a3d297cfe86
K.Rishi Kaushal Reddy <238w1a12f2@vrsec.ac.in>
2026-04-17 16:09:45 +0530

    added backend api live link

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: "https://full-stack-8xym.onrender.com/api"
 })
 API.interceptors.request.use((req) => {
     const token = localStorage.getItem("token")
